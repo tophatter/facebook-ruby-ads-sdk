d73c941559752aa7794570f725628ca9ecb17a69
cte <cestreich@gmail.com>
2016-09-06 19:39:08 -0700

    Don’t need COVERALLS_REPO_TOKEN since we’re public.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'coveralls'
+Coveralls.wear!
+
 require 'webmock/rspec'
 WebMock.disable_net_connect!(allow_localhost: true)
 
@@ -10,12 +13,3 @@ end
 
 require 'facebook_ads'
 FacebookAds.access_token = 'FAKE_ACCESS_TOKEN'
-
-# The COVERALLS_REPO_TOKEN is set in the project setting for TravisCI.
-# https://travis-ci.org/cte/facebook-ads-sdk-ruby/settings
-# https://coveralls.io/github/cte/facebook-ads-sdk-ruby
-if ENV['COVERALLS_PUSH']
-  puts "Read #{ENV['COVERALLS_REPO_TOKEN'].length} character COVERALLS_REPO_TOKEN from ENV."
-  require 'coveralls'
-  Coveralls.wear!
-end
