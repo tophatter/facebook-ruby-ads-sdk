7e3abc922a15f5b7f0577b82664d76cc52739ac0
cte <cestreich@gmail.com>
2016-09-08 00:46:57 -0700

    Add fields to ad insight object.

--- lib/facebook_ads/ad_insight.rb
+++ lib/facebook_ads/ad_insight.rb
@@ -2,9 +2,10 @@ module FacebookAds
   # Ad insights exist for ad accounts, ad campaigns, ad sets, and ads.
   # A lot more can be done here.
   # https://developers.facebook.com/docs/marketing-api/insights/overview
+  # https://developers.facebook.com/docs/marketing-api/insights/fields/v2.7
   class AdInsight < Base
 
-    FIELDS = %w(ad_id objective impressions unique_actions cost_per_unique_action_type clicks cpc cpm ctr spend)
+    FIELDS = %w(account_id campaign_id adset_id ad_id objective impressions unique_actions cost_per_unique_action_type clicks cpc cpm ctr spend)
 
     class << self
       def find(id)
