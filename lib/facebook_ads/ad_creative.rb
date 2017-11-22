0485604d0bb65679805c4f6417ccb2c5d80e7561
Ian Cole <ian.cole@me.com>
2017-11-22 15:43:33 -0800

    Fix rubocop offenses

--- lib/facebook_ads/ad_creative.rb
+++ lib/facebook_ads/ad_creative.rb
@@ -91,14 +91,14 @@ module FacebookAds
         }
       end
 
-      def link(name:, title:, body:, object_url:, link_url:, image_hash:, page_id:)
+      def link(name:, title:, body:, link_url:, image_hash:, page_id:)
         object_story_spec = {
           'page_id' => page_id,
           'link_data' => {
             'name' => title,
             'message' => body,
             'link' => link_url,
-            'image_hash' => image_hash,
+            'image_hash' => image_hash
           }
         }
 
