3326b48c1ba86f763aa8037aeb03e3976a3a028d
Adam Mosharrafa <adammosh@stanford.edu>
2018-10-05 10:30:15 -0700

    Add ad_set

--- lib/facebook_ads/ad_set_activity.rb
+++ lib/facebook_ads/ad_set_activity.rb
@@ -3,8 +3,8 @@
 module FacebookAds
   # https://developers.facebook.com/docs/marketing-api/reference/ad-activity/
   # curl -G
-  # \ -d 'fields=actor_id,actor_name,event_time,event_type,extra_data,translated_event_type'
-  # \ -d 'access_token=<access_token>' \ https://graph.facebook.com/v3.1/<ad_set_id>/activities?
+  #      -d 'fields=actor_id,actor_name,event_time,event_type,extra_data,translated_event_type'
+  #      -d 'access_token=<access_token>' https://graph.facebook.com/v3.1/<ad_set_id>/activities?
   class AdSetActivity < Base
     FIELDS = %w[
       actor_id
@@ -14,5 +14,9 @@ module FacebookAds
       extra_data
       translated_event_type
     ].freeze
+
+    def ad_set
+      @ad_set ||= AdSet.find(ad_set_id)
+    end
   end
 end
