27db814aecb54e6b932f20d221d7b1a92229c03b
Adam Mosharrafa <adammosh@stanford.edu>
2018-11-26 09:21:11 -0800

    Pass date range as param

--- lib/facebook_ads/ad_set.rb
+++ lib/facebook_ads/ad_set.rb
@@ -109,9 +109,9 @@ module FacebookAds
       AdInsight.paginate("/#{id}/insights", query: query)
     end
 
-    # Retrieves activities for ad set in last 24 hours
-    def activities
-      AdSetActivity.get("/#{id}/activities", objectify: true).select { |activity| activity['event_time'] > 1.day.ago }
+    # Retrieves activities for ad set (in last 24 hours by default)
+    def activities(since = 1.day.ago)
+      AdSetActivity.get("/#{id}/activities", objectify: true).select { |activity| activity['event_time'] > since }
     end
   end
 end
