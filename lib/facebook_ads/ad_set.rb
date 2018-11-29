2ff35be460bd9c9341014c0d67f6768783f19993
Adam Mosharrafa <adammosh6@gmail.com>
2018-11-29 11:35:34 -0800

    Activities now fetch range (#34)
    * Now accepts until as parameter
    
    * Remove comment
    
    * Rename params

--- lib/facebook_ads/ad_set.rb
+++ lib/facebook_ads/ad_set.rb
@@ -109,9 +109,9 @@ module FacebookAds
       AdInsight.paginate("/#{id}/insights", query: query)
     end
 
-    # Retrieves activities for ad set (in last 24 hours by default)
-    def activities(since: 1.day.ago)
-      AdSetActivity.get("/#{id}/activities", objectify: true).select { |activity| activity['event_time'] > since }
+    def activities(from: Date.today.beginning_of_day, to: Date.today.end_of_day)
+      query = { since: from, until: to }
+      AdSetActivity.get("/#{id}/activities", query: query, objectify: true)
     end
   end
 end
